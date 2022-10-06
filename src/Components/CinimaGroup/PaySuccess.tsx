3ff20ba847a38e7f754cffcca8854978c436dabb
hungit1993qt <hungit1993qt@gmail.com>
2022-10-06 19:14:49 +0700

    fix

--- src/Components/CinimaGroup/PaySuccess.tsx
+++ src/Components/CinimaGroup/PaySuccess.tsx
@@ -35,7 +35,7 @@ const PaySuccess = (props: Props) => {
               <b>355$</b>
             </div>
             <div className={styles["pay"]}>
-              <img onClick={()=>navigate("/s")} src="images/paysuccess.png" alt="" />
+              <img onClick={()=>navigate("/")} src="images/paysuccess.png" alt="" />
               <span className={styles["title"]}>Payment Success</span>
               <span className={styles["descript"]}>
                 Please check the information from the email, thanks.
