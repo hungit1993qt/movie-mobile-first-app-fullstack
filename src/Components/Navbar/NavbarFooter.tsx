53814491db28cefc68505199b3327bb92895f498
hungit1993qt <hungit1993qt@gmail.com>
2022-10-07 21:59:55 +0700

    fix

--- src/Components/Navbar/NavbarFooter.tsx
+++ src/Components/Navbar/NavbarFooter.tsx
@@ -28,7 +28,45 @@ const NavbarFooter = () => {
             : stylesuser["navbar-user"]
         }
       >
-        <div className={stylesuser["popup-inner"]}></div>
+        <div className={stylesuser["popup-inner"]}>
+          <div className={stylesuser["info-user"]}>
+            <img src="images/avatar.jpg" alt="" />
+            <div className={stylesuser["info"]}>
+              <b>Mr.Hung</b>
+              <span>(Client)</span>
+            </div>
+          </div>
+          <div className={stylesuser["btn-func"]}>
+            <div className={stylesuser["btn-items"]}>
+              <i className="fa fa-info-circle"></i>
+              <b>Information</b>
+            </div>
+            <div className={stylesuser["btn-items"]}>
+              <i className="fa fa-check-circle"></i>
+              <b>List booked</b>
+            </div>
+            <div className={stylesuser["btn-items"]}>
+              <i className="fab fa-facebook-messenger"></i>
+              <b>Messages</b>
+            </div>
+            <div className={stylesuser["btn-items"]}>
+              <i className="fa fa-phone-volume"></i>
+              <b>Hotline</b>
+            </div>
+            <div className={stylesuser["btn-items"]}>
+              <i className="fa fa-mail-bulk"></i>
+              <b>Sent mail</b>
+            </div>
+            <div className={stylesuser["btn-items"]}>
+              <i className="fa fa-cog"></i>
+              <b>Settings</b>
+            </div>
+            <div className={stylesuser["btn-items"]}>
+              <i className="fa fa-sign-out-alt"></i>
+              <b>Logout</b>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
